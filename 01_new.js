d77934d7687909e5bbd9b8c281cccefd56bd7979
dhanushkomari <dhanushkomari@gmail.com>
2020-10-17 13:18:58 +0530

    added 01_new.js to project

---
+++ 01_new.js
@@ -0,0 +1,28 @@
+firstName = 'Dhanush'
+lastName = 'Komari'
+email = 'Dhanushkomari@gmail.com'
+address = 'Nellore'
+pincode = 524344
+salary = 20000
+
+console.log(`
+*********** THE PERSON DETAILS ARE **************
+First Name: ${firstName}
+Second Name: ${lastName}
+email : ${email}
+salary per month : ${salary}
+address: ${address}
+Pincode : ${pincode}
+Permanant address: ${firstName}  ${lastName},
+                ${address},
+                ${pincode}.
+                ${email}
+                        
+*************************************************
+The total package is: ${salary*12}
+*************************************************
+            
+`)
+
+
+
