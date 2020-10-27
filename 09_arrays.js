b52f9fe54074fbdd646a03047bc5955d907500c2
dhanushkomari <dhanushkomari@gmail.com>
2020-10-27 12:15:35 +0530

    fill and filter in arrays added

---
+++ 09_arrays.js
@@ -0,0 +1,10 @@
+// Fill and Filter in arrays //
+
+var testArray = [2,4,6,5,3,1,7,9,90,97,96,98]
+
+console.log(testArray.fill("h",3,6));
+
+
+const myNumber = [21,23,54,56,34,65,676]
+const result = myNumber.filter((num) => (num >= 54))
+console.log(result);
