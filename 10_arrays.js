d0089c1e37695b17a98fd241d97504bd33efe0c4
dhanushkomari <dhanushkomari@gmail.com>
2020-10-27 12:42:54 +0530

    added slice and splice

---
+++ 10_arrays.js
@@ -0,0 +1,11 @@
+// slicing and splice methods in arrays
+
+var users = ["dhanush","sekhar","chintu","chanuky","junnu"]
+
+console.log(users.slice(0,3));
+console.log(users)
+
+
+//splice
+users.splice(0,2,"pubg","erangal")
+console.log(users);
