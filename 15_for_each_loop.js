198e90c4da027188cbe43abaa70c331905bada93
dhanushkomari <dhanushkomari@gmail.com>
2020-10-28 12:01:52 +0530

    for each using callback functions

---
+++ 15_for_each_loop.js
@@ -0,0 +1,11 @@
+//////////////    for each     /////////////////
+
+
+const myStates = ["AP","TN","KA",1947,"TS","KL"]
+
+// myStates.forEach((item, i) => {
+//   console.log(item);
+// });
+
+
+myStates.forEach( (s) => (console.log(s)) )   // using call back functions
