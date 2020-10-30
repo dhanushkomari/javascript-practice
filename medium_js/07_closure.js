1b98e06725e67fbb5ba254312b33115927e6fcd2
dhanushkomari <dhanushkomari@gmail.com>
2020-10-30 15:12:10 +0530

    closure

---
+++ medium_js/07_closure.js
@@ -0,0 +1,25 @@
+// // closures in javascript
+//
+//
+// function init(){
+//     var firstName = "Dhanush"
+//     console.log("i am init");
+//     function sayfirstName(){
+//       console.log(firstName);
+//     }
+//     return sayfirstName;
+// }
+// var value = init();
+// value()
+
+
+function addition(x){
+  return function(y){
+    return x+y;
+  }
+}
+
+var add5 = addition(4);
+console.log(add5(5));          //
+
+console.log(addition(5)(5));  //another way
