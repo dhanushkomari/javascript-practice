58d5be78e9fe71822753e857c9d3e8016a378edd
dhanushkomari <dhanushkomari@gmail.com>
2020-10-31 10:39:35 +0530

    scope of let and var key words

---
+++ advanced_js/01_scope.js
@@ -0,0 +1,11 @@
+// scope in js
+
+//console.log(name);
+var name = "dhanush"
+
+if (true) {
+  let lastName = "Komari";  // let is only exist in the scope{}
+  console.log(lastName);
+  
+}
+// console.log(lastName);   //returns error
