b46c69a5ba1e52d6a9dce24d1bca5d3768355960
dhanushkomari <dhanushkomari@gmail.com>
2020-10-31 10:58:46 +0530

    string methods and printing a string with ' charecter

---
+++ advanced_js/02_template_literals.js
@@ -0,0 +1,8 @@
+var name = `dhanush's`
+console.log(name);
+
+
+console.log(name.charAt(0));
+console.log(name.endsWith("s"));
+console.log(name.includes('hanus'));
+console.log(name.toUpperCase());
