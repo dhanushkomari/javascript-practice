db67298d743f7aacdf8c3e825e8074be5863eb98
dhanushkomari <dhanushkomari@gmail.com>
2020-11-02 11:50:02 +0530

    event loops in javascript

--- advanced_js/09_event_loop.js
+++ advanced_js/09_event_loop.js
@@ -4,12 +4,12 @@ const uno =  () => {
 const dos = () => {
     setTimeout( () => {         //
       console.log("woohoooo");  //----->  executed after three seconds
-    },5000)                     //
+    },6000)                     //
   console.log("I am Two");
 }
 const tres = () => {
   console.log("I am Three");
-  setTimeout( () => {console.log("Hoorrrayyyyy");},4000)
+  setTimeout( () => {console.log("Hoorrrayyyyy");},5000)
 }
 
 dos();
